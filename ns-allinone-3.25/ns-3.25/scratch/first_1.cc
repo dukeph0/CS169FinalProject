15df3a5b4ce0fbb736735d791f2e143151e24f2c
dukeph0 <dpham5161@outlook.com>
2022-03-08 11:19:26 -0800

    Add ns-3

---
+++ ns-allinone-3.25/ns-3.25/scratch/first_1.cc
@@ -0,0 +1,103 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+
+
+int
+main (int argc, char *argv[])
+{
+  // Set time resolution
+  Time::SetResolution (Time::NS);
+  // Enable log components and set log levels
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_ALL);
+
+  NS_LOG_INFO("Creating Topology");
+
+  // Create nodes
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  // Create Channel
+  PointToPointHelper pointToPoint;
+  //pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  //pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  CommandLine cmd;
+  uint32_t nPackets = 1;
+  //uint32_t nDataRate = 1;
+  //uint32_t nDelay = 1;
+  cmd.AddValue("nPackets", "Number of packets to echo", nPackets);
+  //cmd.AddValue("nDataRate", "Number of data rate to echo", nDataRate);
+  //cmd.AddValue("nDelay", "Number of delay to echo", nDelay);
+  cmd.Parse(argc, argv);
+
+  // Create NetDevice and bind them to Channel
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
+
+  // Create InternetStack
+  InternetStackHelper stack;
+  stack.Install (nodes);
+
+  NS_LOG_INFO("Assigning IP Addresses");
+
+  // Set IP network address
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+
+  // Assign IP to NetDevice
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  NS_LOG_INFO("Creating echoServer");
+
+  // Create echo server
+  UdpEchoServerHelper echoServer (9);
+
+  // Install echo server to node 1, mark it application, and set start and stop time
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  // Create echo client and set it attributes
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
+  //echoClient.SetAttribute ("DataRate", UintegerValue (nDataRate));
+  //echoClient.SetAttribute ("Delay", UintegerValue (nDelay));
+
+  // Install echo client to node 0, mark it application, and set start and stop time
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  // Run, destroy, return
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
